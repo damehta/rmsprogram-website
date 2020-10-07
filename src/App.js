02b9b83a4dfa16243f567121721b385040d9d145
Apeksha Mehta <darshan0mehta@gmail.com>
2020-10-06 21:29:23 -0700

    Updates to package json, app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.png';
+// import logo from './logo.png';
 import './App.css';
 import Home from './containers/Home';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import ContactUs from './containers/ContactUs';
+import MeetOurTeam from './containers/MeetOurTeam';
 import Post from './containers/Post';
 import GuestSpeakers from './containers/GuestSpeakers';
+import ProgramInfo from './containers/ProgramInfo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -16,9 +19,11 @@ function App() {
         <Header/>
         <Hero/>
         <Route path="/" exact component={Home} />
-        {/* <Route path="/contact-us" component={ContactUs} /> */}
+        <Route path="/contact-us" component={ContactUs} />
+        <Route path="/meet-our-team" component={MeetOurTeam} />
         <Route path="/post" component={Post} />
         <Route path="/guest-speakers" component={GuestSpeakers} />
+        <Route path="/program-info" component={ProgramInfo} />
       </div>
     </Router>
     
